83cb6810e9db09301b1c4f8876db84411f551a8d
Suleyman Poyraz <zaryob.dev@gmail.com>
2021-01-31 21:54:47 +0300

    Face Recognition fixes.

---
+++ FaceRecognition/src/main.cpp
@@ -0,0 +1,35 @@
+#include <opencv4/opencv2/opencv.hpp>
+#include "FaceDetector.h"
+
+int main(int argc, char **argv) {
+
+    cv::VideoCapture video_capture;
+    if (!video_capture.open(0)) {
+        return 0;
+    }
+
+    FaceDetector face_detector;
+
+    cv::Mat frame;
+    while (true) {
+        video_capture >> frame;
+
+        auto rectangles = face_detector.detect_face_rectangles(frame);
+        cv::Scalar color(0, 128, 0);
+        for(const auto & r : rectangles){
+            cv::rectangle(frame, r, color, 4);
+        }
+
+
+        imshow("Image", frame);
+        int esc_key = 27;
+        if (cv::waitKey(10) == esc_key) {
+            break;
+        }
+    }
+
+    cv::destroyAllWindows();
+    video_capture.release();
+
+    return 0;
+}
